3692f62c7e7a1a0f7cb8af3afd67d943ce99e781
Simo Partinen <simo.partinen@twoday.com>
2024-11-07 23:43:53 +0200

    Revert "feat: added eslint plugin for react-hooks (tentative)"
    This reverts commit befc947c26b8fba77e8dd6cfbe94be04f29e5c11.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
     'plugin:jsx-a11y/recommended',
     'airbnb',
     'plugin:prettier/recommended',
-    'plugin:react-hooks/recommended',
   ],
   rules: {
     curly: ['error', 'all'],
