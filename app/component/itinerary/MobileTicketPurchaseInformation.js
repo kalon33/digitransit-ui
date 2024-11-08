43b3e272bd54b3566f7a2dd39ffdad3f25c181a3
sharhio <sade.harhio@cgi.com>
2024-11-08 16:27:57 +0200

    DT-6494 new waltti ticket link

--- app/component/itinerary/MobileTicketPurchaseInformation.js
+++ app/component/itinerary/MobileTicketPurchaseInformation.js
@@ -21,7 +21,8 @@ export default function MobileTicketPurchaseInformation(
     !fare.isUnknown,
     config.availableTickets,
   );
-  const price = `${fare.price.toFixed(2)} €`.replace('.', ',');
+  const price =
+    fare.price > 0 ? `${fare.price.toFixed(2)} €`.replace('.', ',') : '';
 
   const faresInfo = () => {
     const header = `${intl.formatMessage({
@@ -64,7 +65,7 @@ export default function MobileTicketPurchaseInformation(
             addAnalyticsEvent({ event: 'journey_planner_open_app' })
           }
         >
-          <FormattedMessage id="open-app" />
+          <FormattedMessage id={config.ticketButtonTextId} />
         </ExternalLink>
       </div>
     </div>
