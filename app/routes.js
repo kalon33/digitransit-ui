b3cae9069a35cc9b591451243f633f1932720a25
Vesa Meskanen <vesa.meskanen@cgi.com>
2024-11-11 16:35:08 +0200

    chore: rename
    Near you view shows more than stops

--- app/routes.js
+++ app/routes.js
@@ -224,7 +224,7 @@ export default config => {
             <Route
               getComponent={() =>
                 import(
-                  /* webpackChunkName: "nearyou" */ './component/stopsnearyou/StopsNearYouPage'
+                  /* webpackChunkName: "nearyou" */ './component/nearyou/StopsNearYouPage'
                 ).then(getDefault)
               }
               render={({ Component, props, error }) => {
@@ -244,7 +244,7 @@ export default config => {
               path="(.*)?"
               getComponent={() =>
                 import(
-                  /* webpackChunkName: "itinerary" */ './component/stopsnearyou/StopsNearYouPageMeta'
+                  /* webpackChunkName: "itinerary" */ './component/nearyou/StopsNearYouPageMeta'
                 ).then(getDefault)
               }
             />
