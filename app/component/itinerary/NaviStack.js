e4938ff43a592fb5e58178f07dded98c08a8a300
Janne Antikainen <antiik91@gmail.com>
2024-11-12 08:41:28 +0200

    navistack top value based on card sizes

--- app/component/itinerary/NaviStack.js
+++ app/component/itinerary/NaviStack.js
@@ -4,16 +4,18 @@ import cx from 'classnames';
 import NaviMessage from './NaviMessage';
 
 const NaviStack = ({ messages, handleRemove, cardExpanded, legType }) => {
+  // Card has 4 sizes: first leg collapsed, expanded
+  // and in transit collapsed, expanded.
+  let classPostfix = '';
+  if (legType === 'in-transit' && cardExpanded) {
+    classPostfix = 'expand-transit';
+  } else if (legType === 'in-transit') {
+    classPostfix = 'in-transit';
+  } else if (cardExpanded) {
+    classPostfix = 'expanded';
+  }
   return (
-    <div
-      className={cx(
-        'info-stack',
-        'slide-in',
-        legType === 'in-transit' && cardExpanded
-          ? 'expand-transit'
-          : cardExpanded && 'expanded',
-      )}
-    >
+    <div className={cx('info-stack', 'slide-in', classPostfix)}>
       {messages.map((notification, index) => (
         <NaviMessage
           key={notification.id}
