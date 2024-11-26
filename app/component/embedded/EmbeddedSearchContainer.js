822bc948b6a4806013e4a57bdc6161e8399a1f0b
Vesa Meskanen <vesa.meskanen@cgi.com>
2024-11-26 14:24:32 +0200

    fix: rename embedded search folder, move all related code to the folder

--- app/component/embedded/EmbeddedSearchContainer.js
+++ app/component/embedded/EmbeddedSearchContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { matchShape } from 'found';
-import LazilyLoad, { importLazy } from './LazilyLoad';
+import LazilyLoad, { importLazy } from '../LazilyLoad';
 
 const modules = {
   EmbeddedSearch: () => importLazy(import('./EmbeddedSearch')),
