518423dad338c60955a96ebfec09307d4e06126c
Vesa Meskanen <vesa.meskanen@cgi.com>
2024-12-02 15:22:55 +0200

    feat: compute planar leg geometry

--- app/component/itinerary/navigator/NaviContainer.js
+++ app/component/itinerary/navigator/NaviContainer.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import polyUtil from 'polyline-encoded';
 import { legTime } from '../../../util/legUtils';
+import { GeodeticToEcef, GeodeticToEnu } from '../../../util/geo-utils';
 import { itineraryShape, relayShape } from '../../../util/shapes';
 import NaviBottom from './NaviBottom';
 import NaviCardContainer from './NaviCardContainer';
@@ -10,20 +12,38 @@ function NaviContainer(
   { itinerary, focusToLeg, relayEnvironment, setNavigation, mapRef },
   { getStore },
 ) {
-  const { legs } = itinerary;
+  const [planarLegs, setPlanarLegs] = useState([]);
+
   const position = getStore('PositionStore').getLocationState();
 
+  useEffect(() => {
+    const { lat, lon } = itinerary.legs[0].from;
+    const origin = GeodeticToEcef(lat, lon);
+    const legs = itinerary.legs.map(leg => {
+      const geometry = polyUtil.decode(leg.legGeometry.points);
+      return {
+        ...leg,
+        geometry: geometry.map(p => GeodeticToEnu(p[0], p[1], origin)),
+      };
+    });
+    setPlanarLegs(legs);
+  }, [itinerary.legs]);
+
   const { realTimeLegs, time, isPositioningAllowed } = useRealtimeLegs(
-    legs,
+    planarLegs,
     mapRef,
     relayEnvironment,
   );
+
+  if (!realTimeLegs.length) {
+    return null;
+  }
 
   // recompute estimated arrival
   let lastTransitLeg;
   let arrivalChange = 0;
 
-  legs.forEach(leg => {
+  realTimeLegs.forEach(leg => {
     if (leg.transitLeg) {
       lastTransitLeg = leg;
     }
@@ -36,7 +56,8 @@ function NaviContainer(
     arrivalChange = legTime(rtLeg.end) - legTime(lastTransitLeg.end);
   }
 
-  const arrivalTime = legTime(legs[legs.length - 1].end) + arrivalChange;
+  const arrivalTime =
+    legTime(realTimeLegs[realTimeLegs.length - 1].end) + arrivalChange;
 
   return (
     <>
