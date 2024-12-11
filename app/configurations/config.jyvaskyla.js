4010613c89159b2253fd72ca85f2ed9b2b60b9c6
Vesa Meskanen <vesa.meskanen@cgi.com>
2024-12-11 11:26:00 +0200

    chore: enable analytics for jyväskylä dev testing

--- app/configurations/config.jyvaskyla.js
+++ app/configurations/config.jyvaskyla.js
@@ -179,4 +179,5 @@ export default configMerger(walttiConfig, {
   // Notice! Turning on this setting forces the search for car routes (for the CO2 comparison only).
   showCO2InItinerarySummary: true,
   sendAnalyticsCustomEventGoals: true,
+  devAnalytics: true,
 });
