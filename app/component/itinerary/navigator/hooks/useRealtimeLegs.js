f34d35e5a9dbdd00a1772af9b05b81fc7ed0287c
Vesa Meskanen <vesa@realsoft.com>
2024-12-22 13:00:01 +0200

    Revert "fix: remove unnecessary nested copy"
    This reverts commit 1b746b2f2f118d3804422a5b28819e64c89fb20e.

--- app/component/itinerary/navigator/hooks/useRealtimeLegs.js
+++ app/component/itinerary/navigator/hooks/useRealtimeLegs.js
@@ -215,7 +215,7 @@ const useRealtimeLegs = (relayEnvironment, initialLegs = []) => {
           },
         };
       }
-      return { ...l };
+      return { ...l, start: { ...l.start }, end: { ...l.end } };
     });
     // shift non-transit-legs to match possibly changed transit legs
     matchLegEnds(rtLegs);
