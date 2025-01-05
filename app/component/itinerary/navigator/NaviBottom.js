24eb25a5adfea6ef1037f2fa39d89e68d1d34538
Vesa Meskanen <vesa@realsoft.com>
2025-01-05 08:42:24 +0200

    Fix: remove unnecessary div

--- app/component/itinerary/navigator/NaviBottom.js
+++ app/component/itinerary/navigator/NaviBottom.js
@@ -10,42 +10,40 @@ export default function NaviBottom(
 ) {
   const remainingDuration = Math.ceil((arrival - time) / 60000); // ms to minutes
   return (
-    <div className="navi-bottom-sheet">
-      <div className="navi-bottom-controls">
-        <button
-          type="button"
-          onClick={() => setNavigation(false)}
-          className="navi-close-button"
-        >
-          <FormattedMessage id="navigation-quit" />
-        </button>
+    <div className="navi-bottom-controls">
+      <button
+        type="button"
+        onClick={() => setNavigation(false)}
+        className="navi-close-button"
+      >
+        <FormattedMessage id="navigation-quit" />
+      </button>
 
-        {remainingDuration >= 0 && (
-          <div className="navi-time">
-            <span>
-              <FormattedMessage
-                id="travel-time"
-                values={{ min: remainingDuration }}
-              />
-            </span>
-            <span className="navi-daytime">{epochToTime(arrival, config)}</span>
-          </div>
-        )}
-        {config.ticketLink && (
-          <button type="button" className="navi-ticket-button">
-            <a
-              onClick={e => {
-                e.stopPropagation();
-              }}
-              href={config.ticketLink}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FormattedMessage id="navigation-ticket" />
-            </a>
-          </button>
-        )}
-      </div>
+      {remainingDuration >= 0 && (
+        <div className="navi-time">
+          <span>
+            <FormattedMessage
+              id="travel-time"
+              values={{ min: remainingDuration }}
+            />
+          </span>
+          <span className="navi-daytime">{epochToTime(arrival, config)}</span>
+        </div>
+      )}
+      {config.ticketLink && (
+        <button type="button" className="navi-ticket-button">
+          <a
+            onClick={e => {
+              e.stopPropagation();
+            }}
+            href={config.ticketLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FormattedMessage id="navigation-ticket" />
+          </a>
+        </button>
+      )}
     </div>
   );
 }
