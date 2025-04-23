b83dbbaa91ef9cb4e83d2aecc5b322f3f4c167d8
Vesa Meskanen <vesa@realsoft.com>
2025-04-23 09:55:04 +0300

    chore: refactor RelativeDuration

--- app/component/itinerary/RelativeDuration.js
+++ app/component/itinerary/RelativeDuration.js
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { FormattedMessage, intlShape } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 
-function RelativeDuration(props) {
-  const hours = Math.floor(props.duration / 3600000);
-  const mins = Math.floor(props.duration / 60000 - hours * 60);
+function RelativeDuration({ duration }) {
+  const hours = Math.floor(duration / 3600000);
+  const mins = Math.floor(duration / 60000 - hours * 60);
   if (hours >= 1) {
     return (
       <FormattedMessage
@@ -21,12 +21,6 @@ function RelativeDuration(props) {
   );
 }
 
-RelativeDuration.contextTypes = {
-  intl: intlShape.isRequired, // eslint-disable-line react/no-typos
-};
-
-RelativeDuration.propTypes = {
-  duration: PropTypes.number.isRequired,
-};
+RelativeDuration.propTypes = { duration: PropTypes.number.isRequired };
 
 export default RelativeDuration;
