4c24ddea7871d5476913328acb9269695d432965
sharhio <sade.harhio@cgi.com>
2025-04-30 13:27:56 +0300

    support replacing default sprites

--- app/configurations/config.matka.js
+++ app/configurations/config.matka.js
@@ -54,6 +54,8 @@ export default {
 
   favicon: './app/configurations/images/matka/matka-favicon.svg',
 
+  themeSprites: 'assets/svg-sprite.matka.svg',
+
   colors: {
     primary: '#000',
     iconColors: {
