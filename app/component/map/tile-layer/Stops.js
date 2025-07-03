bca7b9fab0bfe186acc8f997aca8393b28b74219
Ville Pihlava <ville.pihlava@cgi.com>
2025-07-03 14:25:08 +0300

    Fix hilight spelling to highlight.

--- app/component/map/tile-layer/Stops.js
+++ app/component/map/tile-layer/Stops.js
@@ -47,9 +47,9 @@ class Stops {
   static getName = () => 'stop';
 
   drawStop(feature, isHybrid, zoom, minZoom) {
-    const isHilighted =
-      this.tile.hilightedStops &&
-      this.tile.hilightedStops.includes(feature.properties.gtfsId);
+    const isHighlighted =
+      this.tile.highlightedStops &&
+      this.tile.highlightedStops.includes(feature.properties.gtfsId);
     let hasTrunkRoute = false;
     let hasLocalTramRoute = false;
     if (
@@ -79,7 +79,7 @@ class Stops {
         drawHybridStopIcon(
           this.tile,
           feature.geom,
-          isHilighted,
+          isHighlighted,
           this.config.colors.iconColors,
           hasTrunkRoute,
         );
@@ -101,7 +101,7 @@ class Stops {
         !isNull(feature.properties.platform)
           ? feature.properties.platform
           : false,
-        isHilighted,
+        isHighlighted,
         !!(
           feature.properties.type === 'FERRY' &&
           !isNull(feature.properties.code)
@@ -140,16 +140,16 @@ class Stops {
           this.features = [];
 
           // draw highlighted stops on lower zoom levels
-          const hasHilightedStops = !!(
-            this.tile.hilightedStops &&
-            this.tile.hilightedStops.length &&
-            this.tile.hilightedStops[0]
+          const hasHighlightedStops = !!(
+            this.tile.highlightedStops &&
+            this.tile.highlightedStops.length &&
+            this.tile.highlightedStops[0]
           );
 
           if (
             vt.layers.stops != null &&
             (this.tile.coords.z >= this.config.stopsMinZoom ||
-              hasHilightedStops)
+              hasHighlightedStops)
           ) {
             const featureByCode = {};
             const hybridGtfsIdByCode = {};
@@ -172,8 +172,8 @@ class Stops {
                   // if under zoom level limit, only draw highlighted stops on near you page
                   this.tile.coords.z < this.config.stopsMinZoom &&
                   !(
-                    hasHilightedStops &&
-                    this.tile.hilightedStops.includes(f.properties.gtfsId)
+                    hasHighlightedStops &&
+                    this.tile.highlightedStops.includes(f.properties.gtfsId)
                   )
                 ) {
                   continue; // eslint-disable-line no-continue
@@ -204,14 +204,14 @@ class Stops {
                       prevFeature.properties.type === 'BUS' ? f : prevFeature;
                     hybridGtfsIdByCode[featWithBus.properties.code] =
                       featWithBus.properties.gtfsId;
-                    // Also change hilighted stopId to the stop with type = BUS in hybrid stop cases
+                    // Also change highlighted stopId to the stop with type = BUS in hybrid stop cases
                     if (
-                      this.tile.hilightedStops &&
-                      this.tile.hilightedStops.includes(
+                      this.tile.highlightedStops &&
+                      this.tile.highlightedStops.includes(
                         featWithoutBus.properties.gtfsId,
                       )
                     ) {
-                      this.tile.hilightedStops = [
+                      this.tile.highlightedStops = [
                         featWithBus.properties.gtfsId,
                       ];
                     }
@@ -264,25 +264,27 @@ class Stops {
                 this.stopsToShowCheck(feature, true)
               ) {
                 [[feature.geom]] = feature.loadGeometry();
-                const isHilighted =
-                  this.tile.hilightedStops &&
-                  this.tile.hilightedStops.includes(feature.properties.gtfsId);
+                const isHighlighted =
+                  this.tile.highlightedStops &&
+                  this.tile.highlightedStops.includes(
+                    feature.properties.gtfsId,
+                  );
                 this.features.unshift(pick(feature, ['geom', 'properties']));
                 if (
                   isHybridStation &&
-                  (isHilighted ||
+                  (isHighlighted ||
                     this.tile.coords.z >= this.config.terminalStopsMinZoom)
                 ) {
                   drawHybridStationIcon(
                     this.tile,
                     feature.geom,
-                    isHilighted,
+                    isHighlighted,
                     this.config.colors.iconColors,
                   );
                 }
                 if (
                   !isHybridStation &&
-                  (isHilighted ||
+                  (isHighlighted ||
                     this.tile.coords.z >= this.config.terminalStopsMinZoom) &&
                   shouldRenderTerminalIcon(
                     feature.properties.type,
@@ -294,7 +296,7 @@ class Stops {
                     this.tile,
                     feature.geom,
                     feature.properties.type,
-                    isHilighted,
+                    isHighlighted,
                   );
                 }
               }
