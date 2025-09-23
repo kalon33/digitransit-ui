fa3906f4735c26395ea7f54c8c851f69072bbd86
Vesa Meskanen <vesa@realsoft.com>
2025-09-23 10:29:22 +0300

    fix: citybike marker name on itinerary map rendering

--- app/component/map/non-tile-layer/VehicleMarker.js
+++ app/component/map/non-tile-layer/VehicleMarker.js
@@ -61,7 +61,7 @@ export default class VehicleMarker extends React.Component {
     const vehicleCapacity = getVehicleCapacity(config, rental?.network);
     const iconName = `${getRentalNetworkIcon(
       getRentalNetworkConfig(rental.network, config),
-    )}-stop`;
+    )}-lollipop`;
 
     return !transit && zoom <= config.stopsSmallMaxZoom
       ? L.divIcon({
