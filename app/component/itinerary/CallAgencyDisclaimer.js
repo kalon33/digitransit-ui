63e59056d358c23d12482887f8290552db5279a0
sharhio <sade.harhio@cgi.com>
2025-09-25 13:08:47 +0300

    initial on demand service info page

--- app/component/itinerary/CallAgencyDisclaimer.js
+++ app/component/itinerary/CallAgencyDisclaimer.js
@@ -1,20 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 import Icon from '../Icon';
+import OnDemandInfo from './OnDemandInfo';
+import {
+  agencyShape,
+  routeShape,
+  pickupBookingInfoShape,
+} from '../../util/shapes';
 
-export default function RouteDisclaimer({
+export default function CallAgencyDisclaimer({
   textId,
   text,
   values,
   href,
   linkText,
   header,
+  routeNumber,
+  pickupBookingInfo,
+  agency,
+  route,
+  mobile,
 }) {
+  const [infoOpenState, setInfoOpenState] = useState(false);
+
+  const openOnDemandInfo = () => {
+    setInfoOpenState(true);
+  };
+
+  const closeOnDemandInfo = () => {
+    setInfoOpenState(false);
+  };
+
+  if (infoOpenState) {
+    return (
+      <OnDemandInfo
+        routeNumber={routeNumber}
+        route={route}
+        pickupBookingInfo={pickupBookingInfo}
+        agency={agency}
+        onClose={() => closeOnDemandInfo()}
+        mobile={mobile}
+      />
+    );
+  }
+
   return (
     <div className="call-agency-disclaimer-container">
       <div className="icon-container">
-        <Icon className="info" img="icon-icon_info" />
+        <Icon className="info" img="icon_info" />
       </div>
       <div className="disclaimer">
         {header && <h3 className="disclaimer-header">{header}</h3>}
@@ -23,10 +57,7 @@ export default function RouteDisclaimer({
           <button
             type="button"
             className="external-link-button"
-            onClick={e => {
-              e.stopPropagation();
-              window.open(href, '_blank', 'noopener,noreferrer');
-            }}
+            onClick={() => openOnDemandInfo()}
           >
             {linkText}
           </button>
@@ -35,20 +66,30 @@ export default function RouteDisclaimer({
     </div>
   );
 }
-RouteDisclaimer.propTypes = {
+
+CallAgencyDisclaimer.propTypes = {
   textId: PropTypes.string,
   text: PropTypes.string,
   values: PropTypes.objectOf(PropTypes.string),
   href: PropTypes.string,
   linkText: PropTypes.string,
   header: PropTypes.string,
+  routeNumber: PropTypes.node,
+  pickupBookingInfo: pickupBookingInfoShape,
+  agency: agencyShape,
+  route: routeShape.isRequired,
+  mobile: PropTypes.bool,
 };
 
-RouteDisclaimer.defaultProps = {
+CallAgencyDisclaimer.defaultProps = {
   textId: null,
   text: null,
   values: {},
   href: null,
   linkText: null,
   header: null,
+  routeNumber: null,
+  pickupBookingInfo: null,
+  agency: null,
+  mobile: false,
 };
--- app/component/itinerary/CallAgencyLeg.js
+++ app/component/itinerary/CallAgencyLeg.js
@@ -5,14 +5,16 @@ import connectToStores from 'fluxible-addons-react/connectToStores';
 import { legShape, configShape } from '../../util/shapes';
 import TransitLeg from './TransitLeg';
 import CallAgencyDisclaimer from './CallAgencyDisclaimer';
+import RouteNumberContainer from '../RouteNumber';
+import withBreakpoint from '../../util/withBreakpoint';
 
 const CallAgencyLeg = (
-  { leg, currentLanguage, ...props },
+  { leg, currentLanguage, breakpoint, ...props },
   { intl, config },
 ) => {
   const modeClassName = 'call';
   const { route } = leg;
-
+  const mobile = breakpoint === 'small' || breakpoint === 'medium';
   const notification =
     config.showRouteDescNotification && route.desc?.length
       ? { content: route.desc, link: route.url }
@@ -21,6 +23,17 @@ const CallAgencyLeg = (
           link: '',
         };
   const key = `callAgencyNotification-${route.gtfsId}`;
+  const routeNumber = (
+    <RouteNumberContainer
+      route={leg.route}
+      className={`line ${modeClassName}`}
+      mode={modeClassName}
+      vertical
+      withBar
+      isTransitLeg
+      text={leg.route && leg.route.shortName}
+    />
+  );
   return (
     <TransitLeg mode={modeClassName} leg={leg} omitDivider {...props}>
       <CallAgencyDisclaimer
@@ -28,7 +41,11 @@ const CallAgencyLeg = (
         text={notification.content}
         href={notification.link}
         linkText={intl.formatMessage({ id: 'extra-info' })}
-        header={intl.formatMessage({ id: 'on-demand-service' })}
+        routeNumber={routeNumber}
+        pickupBookingInfo={leg.pickupBookingInfo}
+        agency={route.agency}
+        route={route}
+        mobile={mobile}
       />
     </TransitLeg>
   );
@@ -39,10 +56,12 @@ CallAgencyLeg.propTypes = {
   index: PropTypes.number.isRequired,
   showRouteDescNotification: PropTypes.bool,
   currentLanguage: PropTypes.string.isRequired,
+  breakpoint: PropTypes.string,
 };
 
 CallAgencyLeg.defaultProps = {
   showRouteDescNotification: false,
+  breakpoint: undefined,
 };
 
 CallAgencyLeg.contextTypes = {
@@ -50,8 +69,10 @@ CallAgencyLeg.contextTypes = {
   intl: intlShape.isRequired,
 };
 
+const CallAgencyLegWithBreakpoint = withBreakpoint(CallAgencyLeg);
+
 const connectedComponent = connectToStores(
-  CallAgencyLeg,
+  CallAgencyLegWithBreakpoint,
   ['PreferencesStore'],
   context => ({
     currentLanguage: context.getStore('PreferencesStore').getLanguage(),
