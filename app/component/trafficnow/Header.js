9e39214afc195b3375d83f66099f3284ddafff68
Vesa Meskanen <vesa@realsoft.com>
2025-09-25 10:43:18 +0300

    fix: style breadcrumb path properly

--- app/component/trafficnow/Header.js
+++ app/component/trafficnow/Header.js
@@ -1,11 +1,18 @@
 import { FormattedMessage } from 'react-intl';
 import React from 'react';
+import Link from 'found/Link';
+import Icon from '../Icon';
 
 export default function Header() {
   return (
     <div className="trafficnow-header">
       <span className="tn-breadcrumb">
-        <FormattedMessage id="trafficnow-bread" />{' '}
+        <Link to="/">
+          <FormattedMessage id="trafficnow-bread" />
+        </Link>
+        &nbsp;
+        <Icon img="icon_arrow-dropdown" className="crumbarrow" />
+        &nbsp;
         <FormattedMessage id="trafficnow" />
       </span>
       <h2>
