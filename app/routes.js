84bfe2629e6cbbf6622fae40a572c92823f6a3e1
Vesa Meskanen <vesa@realsoft.com>
2025-09-25 11:03:00 +0300

    chore: render traffcinow only in dev

--- app/routes.js
+++ app/routes.js
@@ -35,6 +35,10 @@ export const historyMiddlewares = [queryMiddleware];
 
 export const render = createRender({});
 
+const IS_DEV =
+  process.env.RUN_ENV === 'development' ||
+  process.env.NODE_ENV !== 'production';
+
 export default config => {
   const indexPageComponents = {
     title: (
@@ -524,14 +528,16 @@ export default config => {
       {config.indexPath !== '' && (
         <Redirect from="/" to={`/${config.indexPath}`} />
       )}
-      <Route
-        path={TRAFFICNOW}
-        getComponent={() =>
-          import(
-            /* webpackChunkName: "trafficnow" */ './component/trafficnow/TrafficNow'
-          ).then(getDefault)
-        }
-      />
+      {IS_DEV && (
+        <Route
+          path={TRAFFICNOW}
+          getComponent={() =>
+            import(
+              /* webpackChunkName: "trafficnow" */ './component/trafficnow/TrafficNow'
+            ).then(getDefault)
+          }
+        />
+      )}
       {/* For all the rest render 404 */}
       <Route path="*" Component={Error404} />
     </Route>
