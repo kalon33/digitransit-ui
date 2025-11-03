1656e5b84edd97726682310298bd5e80ac44efdd
Vesa Meskanen <vesa@realsoft.com>
2025-11-03 15:38:49 +0200

    fix: convert routepagemap to functional component
    It used deprecated react methods.

--- app/component/map/RoutePageMap.js
+++ app/component/map/RoutePageMap.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { createFragmentContainer, graphql } from 'react-relay';
 import { matchShape } from 'found';
 import connectToStores from 'fluxible-addons-react/connectToStores';
@@ -22,161 +22,165 @@ import { boundWithMinimumArea } from '../../util/geo-utils';
 import { getMapLayerOptions } from '../../util/mapLayerUtils';
 import CookieSettingsButton from '../CookieSettingsButton';
 
-class RoutePageMap extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      trackVehicle: !!this.props.match.params.tripId, // map follows vehicle
-    };
-  }
+function RoutePageMap(
+  {
+    match,
+    pattern,
+    lat,
+    lon,
+    breakpoint,
+    mapLayers,
+    mapLayerOptions,
+    trip,
+    error,
+  },
+  { config },
+) {
+  const { tripId } = match.params;
+  const [trackVehicle, setTrackVehicle] = useState(!!tripId);
+  const tripIdRef = useRef();
+  const mwtRef = useRef();
+  const latRef = useRef();
+  const lonRef = useRef();
+  const bounds = useRef();
+  const code = useRef();
 
-  static propTypes = {
-    match: matchShape.isRequired,
-    pattern: patternShape.isRequired,
-    lat: PropTypes.number,
-    lon: PropTypes.number,
-    breakpoint: PropTypes.string.isRequired,
-    mapLayers: mapLayerShape.isRequired,
-    mapLayerOptions: mapLayerOptionsShape.isRequired,
-    trip: PropTypes.shape({ gtfsId: PropTypes.string }),
-    error: errorShape,
-  };
-
-  static defaultProps = {
-    trip: null,
-    lat: undefined,
-    lon: undefined,
-    error: undefined,
-  };
-
-  static contextTypes = {
-    config: configShape.isRequired,
-  };
-
-  // eslint-disable-next-line camelcase
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    if (nextProps.match.params.tripId !== this.tripId) {
-      this.setState({
-        trackVehicle: !!nextProps.match.params.tripId,
-      });
-    }
-    if (
-      nextProps.match.params.tripId !== this.tripId ||
-      nextProps.pattern.code !== this.code
-    ) {
-      // clear tracking so that map can focus on desired things
-      this.mwtRef?.disableMapTracking();
-    }
-  }
-
-  setMWTRef = ref => {
-    this.mwtRef = ref;
-  };
-
-  stopTracking = () => {
-    // filter events which occur when map moves by changed props
-    if (this.tripId === this.props.match.params.tripId) {
-      // user wants to navigate, allow it
-      this.setState({ trackVehicle: false });
-    }
-  };
-
-  componentDidMount() {
+  useEffect(() => {
     // Throw error in client side if relay fails to fetch data
-    if (this.props.error && !this.props.pattern) {
-      throw this.props.error.message;
+    if (error && !pattern) {
+      throw error.message;
     }
+  }, []);
+
+  if (!pattern) {
+    return null;
   }
 
-  render() {
-    const { pattern, lat, lon, match, breakpoint, mapLayers, mapLayerOptions } =
-      this.props;
-    if (!pattern) {
-      return false;
+  useEffect(() => {
+    if (tripId !== tripIdRef) {
+      setTrackVehicle(!!tripId);
+      mwtRef.current?.disableMapTracking();
     }
-    const { tripId } = match.params;
-    const mwtProps = {};
-    if (tripId && lat && lon) {
-      // already getting vehicle pos
-      if (this.state.trackVehicle) {
-        mwtProps.lat = lat;
-        mwtProps.lon = lon;
-        this.lat = lat;
-        this.lon = lon;
-        if (this.tripId !== tripId) {
-          setTimeout(() => {
-            this.tripId = tripId;
-          }, 500);
-        }
-      } else {
-        mwtProps.lat = this.lat;
-        mwtProps.lon = this.lon;
-      }
-      mwtProps.zoom = 16;
-    } else {
-      if (this.code !== pattern.code || !this.bounds) {
-        let filteredPoints;
-        if (pattern.geometry) {
-          filteredPoints = pattern.geometry.filter(
-            point => point.lat !== null && point.lon !== null,
-          );
-        }
-        this.bounds = boundWithMinimumArea(
-          (filteredPoints || pattern.stops).map(p => [p.lat, p.lon]),
-        );
-        this.code = pattern.code;
-      }
-      if (this.tripId) {
-        // changed back to route view, force update
-        this.mwtRef?.forceRefresh();
-        this.tripId = undefined;
-      }
-      mwtProps.bounds = this.bounds;
-    }
-    const tripSelected =
-      lat && lon && this.props.trip?.gtfsId && isActiveDate(pattern);
-    const leafletObjs = [
-      <RouteLine
-        key="line"
-        pattern={pattern}
-        vehiclePosition={tripSelected ? { lat, lon } : null}
-      />,
-    ];
-    if (isActiveDate(pattern)) {
-      leafletObjs.push(
-        <VehicleMarkerContainer
-          key="vehicles"
-          direction={pattern.directionId}
-          pattern={pattern.code}
-          headsign={pattern.headsign}
-          topics={[pattern.route]}
-        />,
-      );
-    }
+  }, [tripId]);
 
-    /* eslint-disable jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */
-    return (
-      <MapWithTracking
-        {...mwtProps}
-        className="full"
-        leafletObjs={leafletObjs}
-        mapLayers={mapLayers}
-        mapLayerOptions={mapLayerOptions}
-        onStartNavigation={this.stopTracking}
-        onMapTracking={this.stopTracking}
-        setMWTRef={this.setMWTRef}
-      >
-        {breakpoint !== 'large' && (
-          <BackButton
-            icon="icon_arrow-collapse--left"
-            iconClassName="arrow-icon"
-          />
-        )}
-        {this.context.config.useCookiesPrompt && <CookieSettingsButton />}
-      </MapWithTracking>
+  useEffect(() => {
+    if (pattern.code !== code) {
+      mwtRef.current?.disableMapTracking();
+    }
+  }, [pattern.code]);
+
+  const setMWTRef = ref => {
+    mwtRef.current = ref;
+  };
+
+  const stopTracking = () => {
+    // filter events which occur when map moves by changed props
+    if (tripIdRef.current === tripId) {
+      // user wants to navigate, allow it
+      setTrackVehicle(false);
+    }
+  };
+
+  const mwtProps = {};
+  if (tripId && lat && lon) {
+    // already getting vehicle pos
+    if (trackVehicle) {
+      mwtProps.lat = lat;
+      mwtProps.lon = lon;
+      latRef.current = lat;
+      lonRef.current = lon;
+      if (tripIdRef.current !== tripId) {
+        setTimeout(() => {
+          tripIdRef.current = tripId;
+        }, 500);
+      }
+    } else {
+      mwtProps.lat = latRef.current;
+      mwtProps.lon = lonRef.current;
+    }
+    mwtProps.zoom = 16;
+  } else {
+    if (code.current !== pattern.code || !bounds.current) {
+      let filteredPoints;
+      if (pattern.geometry) {
+        filteredPoints = pattern.geometry.filter(
+          point => point.lat !== null && point.lon !== null,
+        );
+      }
+      bounds.current = boundWithMinimumArea(
+        (filteredPoints || pattern.stops).map(p => [p.lat, p.lon]),
+      );
+      code.current = pattern.code;
+    }
+    if (tripIdRef.current) {
+      // changed back to route view, force update
+      mwtRef.current?.forceRefresh();
+      tripIdRef.current = undefined;
+    }
+    mwtProps.bounds = bounds.current;
+  }
+  const tripSelected = lat && lon && trip?.gtfsId && isActiveDate(pattern);
+  const leafletObjs = [
+    <RouteLine
+      key="line"
+      pattern={pattern}
+      vehiclePosition={tripSelected ? { lat, lon } : null}
+    />,
+  ];
+  if (isActiveDate(pattern)) {
+    leafletObjs.push(
+      <VehicleMarkerContainer
+        key="vehicles"
+        direction={pattern.directionId}
+        pattern={pattern.code}
+        headsign={pattern.headsign}
+        topics={[pattern.route]}
+      />,
     );
   }
+  /* eslint-disable jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */
+  return (
+    <MapWithTracking
+      {...mwtProps}
+      className="full"
+      leafletObjs={leafletObjs}
+      mapLayers={mapLayers}
+      mapLayerOptions={mapLayerOptions}
+      onStartNavigation={stopTracking}
+      onMapTracking={stopTracking}
+      setMWTRef={setMWTRef}
+    >
+      {breakpoint !== 'large' && (
+        <BackButton
+          icon="icon_arrow-collapse--left"
+          iconClassName="arrow-icon"
+        />
+      )}
+      {config.useCookiesPrompt && <CookieSettingsButton />}
+    </MapWithTracking>
+  );
 }
+
+RoutePageMap.propTypes = {
+  match: matchShape.isRequired,
+  pattern: patternShape.isRequired,
+  lat: PropTypes.number,
+  lon: PropTypes.number,
+  breakpoint: PropTypes.string.isRequired,
+  mapLayers: mapLayerShape.isRequired,
+  mapLayerOptions: mapLayerOptionsShape.isRequired,
+  trip: PropTypes.shape({ gtfsId: PropTypes.string }),
+  error: errorShape,
+};
+
+RoutePageMap.defaultProps = {
+  trip: null,
+  lat: undefined,
+  lon: undefined,
+  error: undefined,
+};
+
+RoutePageMap.contextTypes = { config: configShape.isRequired };
 
 const RoutePageMapWithVehicles = connectToStores(
   withBreakpoint(RoutePageMap),
