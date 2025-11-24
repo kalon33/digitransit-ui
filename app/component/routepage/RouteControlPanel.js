626ba38d4099f3c21d9951c678a002963fc98f75
Heikki Vuorinen <hjvuor@icloud.com>
2025-11-24 09:09:30 +0200

    implement RoutePatternSelect using downshift

--- app/component/routepage/RouteControlPanel.js
+++ app/component/routepage/RouteControlPanel.js
@@ -10,7 +10,7 @@ import { enrichPatterns } from '@digitransit-util/digitransit-util';
 import connectToStores from 'fluxible-addons-react/connectToStores';
 import { configShape } from '../../util/shapes';
 import CallAgencyWarning from './CallAgencyWarning';
-import RoutePatternSelect from './RoutePatternSelect';
+import RoutePatternSelectContainer from './RoutePatternSelectContainer';
 import RouteNotification from './RouteNotification';
 import { DATE_FORMAT, ExtendedRouteTypes } from '../../constants';
 import {
@@ -446,7 +446,7 @@ class RouteControlPanel extends React.Component {
         >
           {routeNotifications}
           {patternId && (
-            <RoutePatternSelect
+            <RoutePatternSelectContainer
               params={match.params}
               route={route}
               onSelectChange={this.onPatternChange}
