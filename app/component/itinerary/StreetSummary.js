d17827eb2e9a37249443bc5c1ad5a3875437a69f
Janne Antikainen <antiik91@gmail.com>
2025-11-28 10:04:27 +0200

    simplify expression

--- app/component/itinerary/StreetSummary.js
+++ app/component/itinerary/StreetSummary.js
@@ -9,7 +9,7 @@ import { displayDistance } from '../../util/geo-utils';
 
 export default function StreetSummary(props, { config, intl }) {
   const distance = displayDistance(props.distance, config, intl.formatNumber);
-  const icon = `${props.icon || 'icon_walk'}`;
+  const icon = props.icon || 'icon_walk';
   const duration = durationToString(props.duration * 1000);
   return (
     <span className={cx(props.className)} style={{ whiteSpace: 'nowrap' }}>
