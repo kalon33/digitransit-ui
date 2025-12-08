5726275b24e2048031969261ec748187b657531b
sharhio <sade.harhio@cgi.com>
2025-12-08 14:49:25 +0200

    fix some legs missing viapoint marker and address

--- app/component/itinerary/BikeParkLeg.js
+++ app/component/itinerary/BikeParkLeg.js
@@ -45,6 +45,7 @@ const BikeParkLeg = (
         bikePark
         index={index}
         modeClassName="walk"
+        viaType={leg.from.viaLocationType}
       />
       <div className="small-9 columns itinerary-instruction-column">
         <div className={cx('itinerary-leg-first-row', 'bicycle')}>
