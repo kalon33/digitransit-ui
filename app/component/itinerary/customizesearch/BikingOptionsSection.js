7ecadef2bdd39752fee305eec2a40e5af6c58274
Vesa Meskanen <vesa@realsoft.com>
2026-02-21 18:33:13 +0200

    chore: refactor biking options

--- app/component/itinerary/customizesearch/BikingOptionsSection.js
+++ app/component/itinerary/customizesearch/BikingOptionsSection.js
@@ -1,7 +1,5 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { matchShape } from 'found';
-import { intlShape } from 'react-intl';
 import { saveRoutingSettings } from '../../../action/SearchSettingsActions';
 import SearchSettingsDropdown, {
   getFiveStepOptionsNumerical,
@@ -11,60 +9,45 @@ import { addAnalyticsEvent } from '../../../util/analyticsUtils';
 import { findNearestOption } from '../../../util/planParamUtil';
 import { settingsShape } from '../../../util/shapes';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class BikingOptionsSection extends React.Component {
-  render() {
-    const { defaultSettings, bikeSpeed, overrideStyle } = this.props;
-    const { intl } = this.context;
-    const options = getFiveStepOptionsNumerical(this.props.bikeSpeedOptions);
-    const currentSelection =
-      options.find(option => option.value === bikeSpeed) ||
-      options.find(
-        option =>
-          option.value ===
-          findNearestOption(bikeSpeed, this.props.bikeSpeedOptions),
-      );
-    return (
-      <SearchSettingsDropdown
-        name="bike-speed-selector"
-        currentSelection={currentSelection}
-        defaultValue={defaultSettings.bikeSpeed}
-        onOptionSelected={value => {
-          this.context.executeAction(saveRoutingSettings, {
-            bikeSpeed: value,
-          });
-          addAnalyticsEvent({
-            category: 'ItinerarySettings',
-            action: 'ChangeBikingSpeed',
-            name: value,
-          });
-        }}
-        options={options}
-        formatOptions
-        labelText={intl.formatMessage({ id: 'biking-speed' })}
-        translateLabels={false}
-        overrideStyle={overrideStyle}
-      />
+export default function BikingOptionsSection(
+  { bikeSpeed, bikeSpeedOptions, defaultSettings },
+  { executeAction },
+) {
+  const options = getFiveStepOptionsNumerical(bikeSpeedOptions);
+  const currentSelection =
+    options.find(option => option.value === bikeSpeed) ||
+    options.find(
+      option => option.value === findNearestOption(bikeSpeed, bikeSpeedOptions),
     );
-  }
+  return (
+    <SearchSettingsDropdown
+      name="bike-speed-selector"
+      currentSelection={currentSelection}
+      defaultValue={defaultSettings.bikeSpeed}
+      onOptionSelected={value => {
+        executeAction(saveRoutingSettings, {
+          bikeSpeed: value,
+        });
+        addAnalyticsEvent({
+          category: 'ItinerarySettings',
+          action: 'ChangeBikingSpeed',
+          name: value,
+        });
+      }}
+      options={options}
+      formatOptions
+      labelId="biking-speed"
+      translateLabels={false}
+    />
+  );
 }
 
 BikingOptionsSection.propTypes = {
   bikeSpeed: valueShape.isRequired,
   bikeSpeedOptions: PropTypes.arrayOf(PropTypes.number).isRequired,
-  // eslint-disable-next-line
-  overrideStyle: PropTypes.object,
   defaultSettings: settingsShape.isRequired,
 };
 
-BikingOptionsSection.defaultProps = {
-  overrideStyle: undefined,
-};
-
 BikingOptionsSection.contextTypes = {
-  match: matchShape.isRequired,
-  intl: intlShape.isRequired,
   executeAction: PropTypes.func.isRequired,
 };
-
-export default BikingOptionsSection;
