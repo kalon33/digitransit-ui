541b977c05865520aa6e674e5d36708429050ace
Ville Pihlava <ville.pihlava@cgi.com>
2026-04-07 15:06:33 +0300

    Make IS_DEV util function and change configs to use it.

--- app/util/envUtils.js
+++ app/util/envUtils.js
@@ -1,6 +1,18 @@
-/** Check if application is running in a dev environment. RUN_ENV is defined in kubernetes-deploy for dev instances. For running dev locally, NODE_ENV is checked * */
-export const isDevelopmentEnvironment = config => {
+/**
+ * Check if application is running in a dev environment.
+ * RUN_ENV === 'development' in digitransit-kubernetes-deploy for dev instances.
+ * For running dev locally, NODE_ENV is checked.
+ */
+export function IS_DEV() {
   return (
-    config?.RUN_ENV === 'development' || process.env.NODE_ENV === 'development'
+    process.env.RUN_ENV === 'development' ||
+    process.env.NODE_ENV !== 'production'
   );
-};
+}
+
+/**
+ * Check if application is running in a prod environment.
+ */
+export function IS_PROD() {
+  return !IS_DEV();
+}
