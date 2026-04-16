f79ab0000ad7c557a9c920350a664bd3de44cbaf
Vesa Meskanen <vesa@realsoft.com>
2026-04-16 08:43:01 +0300

    fix: unit tests use new intl context provider

--- test/unit/helpers/mock-intl-enzyme.js
+++ test/unit/helpers/mock-intl-enzyme.js
@@ -1,65 +1,61 @@
 /**
- * Components using the react-intl module require access to the intl context.
- * This is not available when mounting single components in Enzyme.
- * These helper functions aim to address that and wrap a valid,
- * English-locale intl context around them.
- *
- * see: https://github.com/yahoo/react-intl/wiki/Testing-with-React-Intl
+ * Components using react-intl need an intl context.
+ * AppIntlProvider already provides that, so tests can wrap with it directly.
  */
-
 import React from 'react';
-import { IntlProvider, intlShape } from 'react-intl';
+import PropTypes from 'prop-types';
 import { mount, shallow } from 'enzyme';
-
+import { intlShape } from 'react-intl';
 import translations from '../../../app/intl/en';
 import { AppIntlProvider } from '../../../app/util/useTranslationsContext';
+import { initFailedFavouriteMessages } from '../../../app/util/messageUtils';
 
-// Create the IntlProvider to retrieve context for wrapping around.
-const getIntl = locale => {
-  const intlProvider = new IntlProvider(
-    { locale, messages: translations[locale] },
-    {},
-  );
-  const { intl } = intlProvider.getChildContext();
-  return intl;
-};
+initFailedFavouriteMessages(translations.en, 'en');
 
-const providers = {
-  en: getIntl('en'),
-  fi: getIntl('fi'),
-  sv: getIntl('sv'),
-};
-
-/**
- * When using React-Intl `injectIntl` on components, props.intl is required.
- */
-const nodeWithIntlProp = (node, locale) =>
-  React.cloneElement(node, { intl: providers[locale] });
+const getMessages = locale => translations[locale] || {};
 
 export const shallowWithIntl = (
   node,
-  { context, ...additionalOptions } = {},
-  locale = 'en',
+  {
+    context = {},
+    locale = 'en',
+    messages = getMessages(locale),
+    ...additionalOptions
+  } = {},
 ) =>
-  shallow(nodeWithIntlProp(node, locale), {
-    context: { ...context, intl: providers[locale] },
+  shallow(node, {
+    context,
     wrappingComponent: AppIntlProvider,
-    wrappingComponentProps: { locale: 'en', messages: translations.en },
+    wrappingComponentProps: { locale, messages },
     ...additionalOptions,
   });
 
 export const mountWithIntl = (
   node,
-  { context, childContextTypes, ...additionalOptions } = {},
-  locale = 'en',
-) =>
-  mount(nodeWithIntlProp(node, locale), {
-    context: { ...context, intl: providers[locale] },
-    childContextTypes: {
-      intl: intlShape,
-      ...childContextTypes,
+  {
+    context = {},
+    childContextTypes = {},
+    locale = 'en',
+    messages = getMessages(locale),
+    ...additionalOptions
+  } = {},
+) => {
+  const fullChildContextTypes = {
+    intl: intlShape,
+    config: PropTypes.object, // 👈 important
+    ...childContextTypes,
+  };
+
+  return mount(
+    <AppIntlProvider locale={locale} messages={messages}>
+      {node}
+    </AppIntlProvider>,
+    {
+      context: {
+        ...context,
+      },
+      childContextTypes: fullChildContextTypes,
+      ...additionalOptions,
     },
-    wrappingComponent: AppIntlProvider,
-    wrappingComponentProps: { intl: providers[locale] },
-    ...additionalOptions,
-  });
+  );
+};
