e7d35f36ad8ad276a354167cdff8f02fe94978fa
Vesa Meskanen <vesa@realsoft.com>
2026-04-27 16:13:34 +0300

    feat: recommended prop to Feedback

--- app/component/itinerary/Feedback.js
+++ app/component/itinerary/Feedback.js
@@ -1,19 +1,24 @@
+import PropTypes from 'prop-types';
 import cx from 'classnames';
 import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import Icon from '../Icon';
 import { useConfigContext } from '../../configurations/ConfigContext';
 
-export default function Feedback({}) { // eslint-disable-line
+export default function Feedback({ recommended }) {
   const { colors } = useConfigContext();
+  const favIcon = recommended
+    ? 'icon_star-with-circle'
+    : 'icon_star-unselected';
   const iconMap = {
     'personalisation-ask': {
-      id: 'icon_star-with-circle',
-      color: '#c53291',
+      img: favIcon,
+      className: cx('favourite', { selected: recommended }),
+      fill: recommended ? '#c53291' : '#FFF',
     },
-    'personalisation-liked': { id: 'icon_thumb', color: colors.primary },
+    'personalisation-liked': { img: 'icon_thumb', color: colors.primary },
     'personalisation-disliked': {
-      id: 'icon_thumb-down',
+      img: 'icon_thumb-down',
       color: colors.primary,
     },
   };
@@ -38,12 +43,7 @@ export default function Feedback({}) { // eslint-disable-line
           'feedback-text-posted': status !== 'personalisation-ask',
         })}
       >
-        <Icon
-          img={iconProps.id}
-          color={iconProps.color}
-          height={1.4}
-          width={1.4}
-        />
+        <Icon {...iconProps} height={1.4} width={1.4} />
         <span>&nbsp;&nbsp;&nbsp;</span>
         <FormattedMessage id={status} />
       </div>
@@ -70,3 +70,8 @@ export default function Feedback({}) { // eslint-disable-line
     </div>
   );
 }
+
+Feedback.propTypes = {
+  recommended: PropTypes.bool,
+  // setFeedback: PropTypes.func.isRequired,
+};
