6642fb4a5df0c063b6134e3dd981ff5c6bc82a32
Vesa Meskanen <vesa@realsoft.com>
2026-05-28 15:50:01 +0300

    chore: remove useless div wrappers

--- app/component/itinerary/SearchSettings.js
+++ app/component/itinerary/SearchSettings.js
@@ -1,7 +1,5 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import cx from 'classnames';
-
 import RightOffcanvasToggle from './RightOffcanvasToggle';
 import DatetimepickerContainer from '../DatetimepickerContainer';
 import { useConfigContext } from '../../configurations/ConfigContext';
@@ -10,29 +8,27 @@ export default function SearchSettings({ toggleSettings }) {
   const config = useConfigContext();
 
   return (
-    <div className={cx(['searchsettings-container'])}>
-      <div className="datetimepicker-container">
-        <DatetimepickerContainer
-          realtime={false}
-          embedWhenClosed={
-            !config.hideItinerarySettings && (
-              <div className="open-advanced-settings">
-                <RightOffcanvasToggle onToggleClick={toggleSettings} />
-              </div>
-            )
-          }
-          embedWhenOpen={
-            <div className="open-embed-container">
-              <div className="open-advanced-settings open-embed">
-                {!config.hideItinerarySettings && (
-                  <RightOffcanvasToggle onToggleClick={toggleSettings} />
-                )}
-              </div>
+    <div className="searchsettings-container">
+      <DatetimepickerContainer
+        realtime={false}
+        embedWhenClosed={
+          !config.hideItinerarySettings && (
+            <div className="open-advanced-settings">
+              <RightOffcanvasToggle onToggleClick={toggleSettings} />
             </div>
-          }
-          color={config.colors.primary}
-        />
-      </div>
+          )
+        }
+        embedWhenOpen={
+          <div className="open-embed-container">
+            <div className="open-advanced-settings open-embed">
+              {!config.hideItinerarySettings && (
+                <RightOffcanvasToggle onToggleClick={toggleSettings} />
+              )}
+            </div>
+          </div>
+        }
+        color={config.colors.primary}
+      />
     </div>
   );
 }
